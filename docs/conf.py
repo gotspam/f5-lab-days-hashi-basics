40b5e1bf5d00f1d29c599e4878a1b7fba42cbddf
gotspam <e.rabago@f5.com>
2020-09-15 09:07:02 -0700

    update git link for reals

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 classname = "F5 And Hashicorp Basics"
 
 # OPTIONAL: The URL to the GitHub Repository for this class
-github_repo = "https://github.com/gotspam/f5-lab-days-hasi-basics"
+github_repo = "https://github.com/gotspam/f5-lab-days-hashi-basics"
 
 # OPTIONAL: Google Analytics
 # googleanalytics_id = 'UA-85156643-4'
